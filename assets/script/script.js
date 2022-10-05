9301198893f5861d478af1bee2ef39639c43e020
Anton Krasnikov <krasnikovanton84@gmail.com>
2022-10-05 16:55:08 -0700

    Add promp and confirm boxes for prerequistes

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,9 +4,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  passwordText.value = password;
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password"); 
+  passwordText.value = password;
+  
 }
 
 // Add event listener to generate button
@@ -14,5 +15,11 @@ generateBtn.addEventListener("click", writePassword);
 ///////////////////////// DO NOT CHANGE ABOVE HERE /////////////////////////
 
 function generatePassword() {
+  let length = window.prompt("Choose a length in boundaries (no less 8 characters and no more then 128 characters)");
+  let isLowercaseIncluded = window.confirm("Include lowercase letters?");
+  let isUpperCaseIncluded = window.confirm("Include uppercase letters?");
+  let isNumeric = window.confirm("Include numbers?");
+  let isSpecCharsIncluded = window.confirm("Include spec characters?");
+  return "p@ssw0rd";
   /////////////////////////// WRITE YOUR CODE HERE /////////////////////////
 }
