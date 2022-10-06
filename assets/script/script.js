33a6f3edc947f7caaa98eca87300ce66006ebcc5
Anton Krasnikov <krasnikovanton84@gmail.com>
2022-10-05 19:03:16 -0700

    Add nothing. saved file

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,8 +6,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password"); 
-  passwordText.value = password;
-  
+  passwordText.value = password; 
 }
 
 // Add event listener to generate button
